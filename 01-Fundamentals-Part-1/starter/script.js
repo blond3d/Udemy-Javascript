c3106d79694f00998748dbf7ffbfd9932e96d662
StevenM_Blond3d <stevenmendez9@gmail.com>
2022-12-24 22:40:35 -0600

    basic operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -33,7 +33,6 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
 let age = 30;
 age = 31;
@@ -46,3 +45,31 @@ job = "teacher";
 
 lastName = "Mendez";
 console.log(lastName);
+
+*/
+//Math operators
+const now = 2037;
+const ageSteven = now - 1993;
+const ageSam = now - 2020;
+console.log(ageSteven, ageSam);
+
+console.log(ageSteven * 2, ageSteven / 2, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 - 2 * 2 * 2
+
+const firstName = "Steven";
+const lastName = "Mendez";
+console.log(firstName + " " + lastName);
+//assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+//comparison operators
+console.log(ageSteven > ageSam); // >, <, >=, <=
+console.log(ageSam >= 18);
+const isFullAge = ageSam >= 18;
+
+console.log(now - 1991 > now - 2019);
