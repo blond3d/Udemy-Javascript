9421742f7f336b0c0af7f217bd1181246ecd8b7d
StevenM_Blond3d <stevenmendez9@gmail.com>
2022-12-24 22:01:36 -0600

    values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,14 @@
 let js = "amazing";
-if (js === "amazing") alert("javascript is fun");
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Steven";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "programmer";
+let myCurrentJob = "teacher";
