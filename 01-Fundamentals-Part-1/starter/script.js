90e2ffeda7c59819dc053b9d570e888714d08924
StevenM_Blond3d <stevenmendez9@gmail.com>
2022-12-24 22:16:36 -0600

    data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,5 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+/*let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
 console.log(23);
@@ -12,3 +12,25 @@ console.log(firstName);
 
 let myFirstJob = "programmer";
 let myCurrentJob = "teacher";
+
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 12);
+// console.log(typeof "Steven");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
