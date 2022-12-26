e0093fe91def938ae7d486cd4687faf8356f1580
StevenM_Blond3d <stevenmendez9@gmail.com>
2022-12-26 08:47:17 -0600

    arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -39,7 +39,6 @@ console.log(appleOrangeJuice);
 
 const num = Number("23");
 
-*/
 ///////////////////////////////////////
 // FUNCTION DECLARATIONS VS EXPRESSIONS
 //////////////////////////////////////
@@ -47,15 +46,34 @@ const num = Number("23");
 //function declaration
 
 function calcAge1(birthYear) {
-	return 2037 - birthYear;
+  return 2037 - birthYear;
 }
 const age1 = calcAge1(1991);
 
 //function expression
 const calcAge2 = function (birthYear) {
-	return 2037 - birthYear;
+  return 2037 - birthYear;
 };
 
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+
+*/
+///////////////////////////////////////
+// ARROW FUNCTION
+//////////////////////////////////////
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+	const age = 2037 - birthYear;
+	const retirement = 65 - age;
+	// return retirement;
+	return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Steven"));
+console.log(yearsUntilRetirement(1980, "Bob"));
