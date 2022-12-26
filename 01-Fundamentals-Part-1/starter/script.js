30b484cddb936d601703b6245c405770818b4d53
StevenM_Blond3d <stevenmendez9@gmail.com>
2022-12-25 22:06:13 -0600

    conditional operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -298,7 +298,7 @@ const shouldDrive = hasDriversLicense && hasGoodVision;
     }
     
     
-    */
+
 const day = "friday";
 
 switch (day) {
@@ -338,3 +338,30 @@ if (day === "monday") {
 } else {
 	console.log("not a valid day");
 }
+*/
+
+////////////////////////////////////////////////////////
+
+// conditional operator
+
+///////////////////////////////////////////////////////
+
+const age = 23;
+age >= 18
+	? console.log("I like to drink wine")
+	: console.log("I like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+	drink2 = "wine";
+} else {
+	drink2 = "water";
+}
+
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
