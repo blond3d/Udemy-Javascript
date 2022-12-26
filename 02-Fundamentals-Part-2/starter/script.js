ee4c6f81ba8c43757fc401cfd62daf5463fb8824
StevenM_Blond3d <stevenmendez9@gmail.com>
2022-12-26 08:19:44 -0600

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 ///////////////////////////////////////
 // Activating Strict Mode
 //////////////////////////////////////
@@ -11,3 +11,31 @@ if (hasDriversLicense) console.log("I can drive :D");
 
 // const interface = "audio";
 // const private = 534;
+*/
+
+///////////////////////////////////////
+// FUNCTIONS
+//////////////////////////////////////
+
+function logger() {
+	console.log("my name is steven");
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+	const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+	//juice is not defined outside of the function
+	return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
