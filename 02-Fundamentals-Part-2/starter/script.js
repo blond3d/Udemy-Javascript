e2a3eff94fd358e99e69f45c0fa9a6adf63ef4bf
StevenM_Blond3d <stevenmendez9@gmail.com>
2022-12-26 07:58:32 -0600

    strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+///////////////////////////////////////
+// Activating Strict Mode
+//////////////////////////////////////
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+// const interface = "audio";
+// const private = 534;
