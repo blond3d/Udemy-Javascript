067cdf55302e1e222d181468e943b36fa5cf356d
StevenM_Blond3d <stevenmendez9@gmail.com>
2022-12-27 09:20:36 -0600

    click events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number!';
@@ -7,3 +8,16 @@ document.querySelector('.number').textContent = '13';
 document.querySelector('.score').textContent = '19';
 document.querySelector('.guess').value = '23';
 console.log(document.querySelector('.guess').value);
+*/
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  }
+});
