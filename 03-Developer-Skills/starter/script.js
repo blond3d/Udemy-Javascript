267de7f85c430d7eee0d39ff0ab66fafad24c5d1
StevenM_Blond3d <stevenmendez9@gmail.com>
2022-12-27 08:03:49 -0600

    challenge 1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+let data1 = [17, 21, 23];
+let data2 = [12, 5, -5, 0, 4];
+
+function printForecast(arr) {
+	let str = "";
+	for (let i = 0; i < arr.length; i++) {
+		str += `${arr[i]}C in ${i + 1} days...`;
+	}
+	console.log(str);
+}
+
+printForecast(data1);
+printForecast(data2);
