de564f34183c19bc8385f4639ac439f3de306c4e
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-01-17 10:46:13 -0600

    WORKING WITH STRINGS PT.1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -628,7 +628,7 @@ if (answer === question.get('correct')) {
 
 //convert map to array
 console.log([...question]);
-*/
+
 
 //////////////////////////
 //////////////////////////////////
@@ -668,3 +668,39 @@ for (const [key, value] of gameEvents) {
     console.log(`[FIRST HALF] ${key}: ${value}`);
   } else console.log(`[SECOND HALF] ${key}: ${value}`);
 }
+*/
+//////////////////////////
+//////////////////////////////////
+//WORKING WITH STRINGS PT.1
+/////////////////////////////////
+
+const airline = 'Tap Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+
+console.log('B737'[0]);
+console.log(airline.length);
+console.log('b737'.length);
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  //B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log('You got the middle seat');
+  } else console.log('You got lucky');
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
