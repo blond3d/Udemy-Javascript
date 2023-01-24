d27c9d52e7d209b9be98b4bb12d0a73138f5613e
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-01-24 10:07:49 -0600

    Closures Practice

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -305,7 +305,7 @@ runOnce();
 
 // console.log(isPrivate);
 console.log(notPrivate);
-*/
+
 
 /////////////////////////////////
 // Closures
@@ -326,3 +326,48 @@ booker();
 booker();
 
 console.dir(booker);
+*/
+
+/////////////////////////////////
+// Closures Practice
+////////////////////////////////
+
+//Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+console.dir(f);
+
+//Re-assigning f function
+h();
+f();
+console.dir(f);
+
+//Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+//Due to closure the inside function prioritizes the variable inside the function scope and not the global scope
+const perGroup = 1000;
+
+boardPassengers(180, 3);
