5947f784bc737074ab95c31c2806e10c2ecd0ad6
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-01-24 10:48:39 -0600

    Challenge 2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -326,7 +326,7 @@ booker();
 booker();
 
 console.dir(booker);
-*/
+
 
 /////////////////////////////////
 // Closures Practice
@@ -371,3 +371,16 @@ const boardPassengers = function (n, wait) {
 const perGroup = 1000;
 
 boardPassengers(180, 3);
+*/
+
+/////////////////////////////////
+// Challenge 2
+////////////////////////////////
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
