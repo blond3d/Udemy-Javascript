164c531f3b880bd21e62b4885c2d9fb2a846cb3a
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-01-24 09:42:44 -0600

    Closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -278,7 +278,7 @@ const poll = {
 document
   .querySelector('.poll')
   .addEventListener('click', poll.registerNewAnswer.bind(poll));
-*/
+
 
 /////////////////////////////////
 // Immediatly invoked function expressions
@@ -305,3 +305,24 @@ runOnce();
 
 // console.log(isPrivate);
 console.log(notPrivate);
+*/
+
+/////////////////////////////////
+// Closures
+////////////////////////////////
+
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
