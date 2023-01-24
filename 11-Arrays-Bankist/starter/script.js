44bf393ffc8b3457bfe32a7b892b0744822ce972
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-01-24 11:34:25 -0600

    At method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -80,7 +80,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //Array Methods
 
 ///////////////////////////////////////////////////////////////////////////////
-
+/*
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 //Slice
@@ -114,9 +114,17 @@ console.log([...arr, ...arr2]);
 //JOIN
 console.log('JOIN');
 console.log(letters.join(' - '));
+*/
 
 ////////////////////////////////////////////////////////////////////////////////
-
-//Array Methods
-
+//At method
 ///////////////////////////////////////////////////////////////////////////////
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at([0]));
+
+//Getting the last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at([-1]));
