48ac31c9c9539f9e412606173ad553c8ee3c74dd
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-01-25 19:48:42 -0600

    map method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -88,7 +88,7 @@ displayMovements(account1.movements);
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -194,7 +194,7 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
-*/
+
 
 ////////////////////////////////////////////////////////////////////////////////
 //Challenge 1
@@ -219,3 +219,32 @@ const checkDogs = function (julia, kate) {
 checkDogs(julia1, kate1);
 console.log('-----------');
 checkDogs(julia2, kate2);
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+//Map Method
+///////////////////////////////////////////////////////////////////////////////
+
+const eurToUsd = 1.1;
+
+// const movementsUsd = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+const movementsUsdArr = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUsdArr, 'Arrow');
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescription = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementsDescription);
