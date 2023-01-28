d9ed943ad080b1f65ce9c88a50ced0b4068604c3
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-01-27 20:15:39 -0600

    Flat and Map

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -499,7 +499,7 @@ const accountFor = function (accounts, user) {
 };
 
 console.log(accountFor(accounts, 'Steven Thomas Williams'));
-*/
+
 
 ////////////////////////////////////////////////////////////////////////////////
 //Some and Every
@@ -526,3 +526,33 @@ const deposit = mov => mov > 0;
 console.log(movements.every(deposit));
 console.log(movements.some(deposit));
 console.log(movements.filter(deposit));
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+//Flat and Map
+///////////////////////////////////////////////////////////////////////////////
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// const accountMovements = accounts.map(acc => acc.movements);
+// console.log(accountMovements);
+// const allMovements = accountMovements.flat();
+// console.log(allMovements);
+// const overalBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+// console.log(overalBalance);
+
+//Flat
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+//flatMap
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
