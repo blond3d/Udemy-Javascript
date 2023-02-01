612aaadaa5be4b9d4de5df69eb82d1f608ff819b
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-01-31 18:01:41 -0600

    Types of events and event handlers

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -162,3 +162,20 @@ logo.classList.contains('c'); //Not Includes
 //Don't use will overwrite all classes
 // logo.className ='jonas'
 */
+
+//////////////////////////////////////////////
+//Types of events and event handlers
+//////////////////////////////////////////////
+
+const h1 = document.querySelector('h1');
+const alertH1 = function (e) {
+  alert('addEventListener: Great you are reading the heading');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great you are reading the heading');
+// };
