60d76174a97fe71a5e1a5e36987f5f36329535a2
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-02-03 07:27:29 -0600

    Sticky navigation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -139,6 +139,16 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+//Sticky Navigation
+const initialCords = section1.getBoundingClientRect();
+console.log(initialCords);
+
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+  if (this.window.scrollY > initialCords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
 //////////////////////////////////////////////
 //Selecting, creating and deleting elements
 //////////////////////////////////////////////
