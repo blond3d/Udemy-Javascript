3a9bdc49745d085b27c61938cf621cffea95d158
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-02-07 21:01:40 -0600

    Prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+///////////////////////////////////
+//Constructor functions and the new operator
+//////////////////////////////////
+
 const Person = function (firstName, birthYear) {
   //Instance property
   this.firstName = firstName;
@@ -24,3 +28,28 @@ const jack = new Person('Jack', 1975);
 
 console.log(matilda, jack);
 console.log(jonas instanceof Person);
+
+///////////////////////////////////
+//Prototypes
+///////////////////////////////////
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
