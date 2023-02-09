7583167a2c2ab3b610b918a653f0a853e75d6da2
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-02-08 19:11:03 -0600

    Prototypal inheritance on built in objects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -53,3 +53,33 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
+
+///////////////////////////////////
+//Prototypal inheritance on built in objects
+///////////////////////////////////
+
+console.log(jonas.__proto__);
+//Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 4, 5, 5, 4, 7, 9, 9]; //new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+
+///////////////////////////////////
+//Challenge 1
+///////////////////////////////////
