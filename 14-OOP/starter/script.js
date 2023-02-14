1dcac32c0a2216948d1b0489bf5a5213e221e4f1
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-02-13 18:16:53 -0600

    setters and getters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -69,7 +69,7 @@ const arr = [3, 4, 5, 5, 4, 7, 9, 9]; //new Array === []
 // console.log(arr.__proto__);
 // console.log(arr.__proto__ === Array.prototype);
 
-console.log(arr.__proto__.__proto__);
+// console.log(arr.__proto__.__proto__);
 
 Array.prototype.unique = function () {
   return [...new Set(this)];
@@ -83,7 +83,7 @@ const h1 = document.querySelector('h1');
 ///////////////////////////////////
 //Challenge 1
 ///////////////////////////////////
-
+/*
 const Car = function (make, speed) {
   this.make = make;
   this.speed = speed + 'km/h';
@@ -107,3 +107,80 @@ mercedes.accelerate();
 bmw.brake();
 mercedes.brake();
 mercedes.brake();
+*/
+
+///////////////////////////////////
+//ES6 Classes
+///////////////////////////////////
+
+//class expression
+// const PersonCl = class{}
+
+//Class declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  //Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  //Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) {
+      this._fullName = name;
+    } else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+
+jessica.greet();
+
+//1. Classes are NOT hoisted
+//2.Classes are first class citizens
+//3. Classes are executed in strict mode
+
+const walter = new PersonCl('Walter White', 1965);
+
+///////////////////////////////////
+//Setters and getters
+///////////////////////////////////
+
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
