a52e72b34febe3b7cf48f32fa47ff2a4e9ccca79
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-02-19 08:45:51 -0600

    Inheritance between classes: Constructor Functions

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -266,3 +266,48 @@ ford.speedUS = 200;
 console.log(ford.speedUS);
 ford.brake();
 console.log(ford.speed);
+
+///////////////////////////////////
+//Inheritance between classes: Constructor Functions
+///////////////////////////////////
+
+const Person = function (firstName, birthYear) {
+  //Instance property
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //Never create method inside constructor function
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+//Linking Prototype
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+console.log(mike);
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__.__proto__);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
+
+///////////////////////////////////
+//Challenge 3
+///////////////////////////////////
