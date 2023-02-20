2e605a98eca6640612cd9262ff20d6e339c0ffc8
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-02-20 09:08:20 -0600

    Inheritance between classes: Object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -352,7 +352,7 @@ tesla.accelerate();
 ///////////////////////////////////
 //Inheritance between classes: ES6 Classes
 ///////////////////////////////////
-
+/*
 class PersonCl {
   constructor(fullName, birthYear) {
     this.fullName = fullName;
@@ -410,3 +410,36 @@ class StudentCL extends PersonCl {
 const martha = new StudentCL('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
+*/
+
+///////////////////////////////////
+//Inheritance between classes: Object.create
+///////////////////////////////////
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
