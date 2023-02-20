c174ebb7faeb19a38caf9d7cb4039ef2d9bef33e
StevenM_Blond3d <stevenmendez9@gmail.com>
2023-02-20 09:20:14 -0600

    Another Class example

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -443,3 +443,47 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+
+///////////////////////////////////
+//Another Class example
+///////////////////////////////////
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  //Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+  approvedLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approvedLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+// acc1.movements.push(250);
+// acc1.movements.push(-250);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+
+console.log(acc1);
+
+console.log(acc1.pin);
